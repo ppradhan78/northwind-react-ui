5b859178f2446b90e76ac7afaeac48bd1f3059b6
ADMIN <ADMIN@DESKTOP-M57JVOT>
2026-01-10 15:29:19 +0530

    Enforce minimum coverage

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
+      lines: 80,
+      functions: 80,
+      branches: 70,
+      statements: 80,
       reportsDirectory: "./coverage",
       exclude: ["node_modules/", "src/test/", "**/*.d.ts", "**/*.config.*"],
     },
